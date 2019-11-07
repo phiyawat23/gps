106ec425c0a3d984180252e9029db641d2378307
a10305-4 <ahkkl31@gmail.com>
2019-11-07 22:54:53 +0700

    Initial commit

---
+++ app/src/main/java/bank/ruts/ac/th/gps/MapsActivity.java
@@ -0,0 +1,60 @@
+package bank.ruts.ac.th.gps;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Location;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GPSTracker gpsTracker;
+    private Location location;
+    double latitude,longtitude;
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        //add gpsTracker
+        gpsTracker = new GPSTracker(getApplicationContext());
+        location = gpsTracker.getLocation();
+
+        latitude = location.getLatitude();
+        longtitude = location.getLongitude();
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng thailand = new LatLng(13.03887, 101.490104);
+        mMap.addMarker(new MarkerOptions().position(thailand).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(thailand));
+    }
+}
